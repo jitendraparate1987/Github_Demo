6999a02d39c5d20b3e814f9c06401dcd23f29665
Jitendra Parate <jitendra.10738478@ltimindtree.com>
2026-04-16 15:05:42 +0530

    Reshuffled for conflict demo

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -12,12 +12,12 @@ public class LoginPage {
     public void open(String url){
         System.out.println("Driver is yet to launch web browser");
         driver.get(url);
-        System.out.println("commit-1-Rebase");
-        System.out.println("commit-2-Rebase");
-        System.out.println("commit-3-Rebase");
-        System.out.println("commit-4-Rebase");
-        System.out.println("commit-5-Rebase");
-        System.out.println("commit-6-Rebase");
+        System.out.println("commit-178-Rebase");
+        System.out.println("commit-254-Rebase");
+        System.out.println("commit-32-Rebase");
+        System.out.println("commit-67-Rebase");
+        System.out.println("commit-55-Rebase");
+        System.out.println("commit-9-Rebase");
 
     }
 
